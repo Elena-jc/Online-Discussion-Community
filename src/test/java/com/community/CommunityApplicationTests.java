d149705db4bd514cfeb266fa61e97c6206eeb356
canj6 <canj78809@gmail.com>
2024-08-30 13:48:11 +0800

    Initial commit

---
+++ src/test/java/com/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
